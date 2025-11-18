233bf34706cbda28463411d3564f50422c94519c
Zohaib479 <don462671@gmail.com>
2025-11-18 18:07:00 +0500

    frontend = next

--- backend/main.py
+++ backend/main.py
@@ -10,17 +10,12 @@ app.add_middleware(
     allow_methods=["*"],  
     allow_headers=["*"],
 )
-
-# Input model
 class TitleInput(BaseModel):
     title: str
-
 @app.post("/recommend")
 def recommend(input: TitleInput):
-    
     # Call CBF recommender
     results = cbf_recommend(input.title)
-
     return {
         "input_title": input.title,
         "recommendations": results
